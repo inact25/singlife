7bbeba4cc776454ca9cd483d707c8c52633ad990
Angga Pixa <anggas25@live.com>
2024-04-20 00:23:12 +0700

    add font style

--- src/components/atom/splash/index.tsx
+++ src/components/atom/splash/index.tsx
@@ -2,9 +2,8 @@ import logo from '@assets/svgs/logo.svg'
 
 const Index = () => {
   return (
-    <div className='flex min-h-screen justify-start items-center'>
+    <div className='flex min-h-screen justify-start text-re items-center bg-singlife-red-800'>
       <img src={logo} alt='splash' />
-      <span className='text-purple-900'>asd</span>
     </div>
   )
 }
