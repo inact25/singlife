d2ac0fa460c25602f2c7d7b0cfb8fe40d118577e
Angga Pixa <anggas25@live.com>
2024-04-19 23:26:12 +0700

    add color style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'singlife-red-800': '#FF0008',
+      'singlife-red-200': '#FFE9EC',
+      'singlife-Turquoise-800': '#19D3C5',
+      'singlife-Turquoise-200': '#B0ECE6',
+      'singlife-Purple-800': '#883E89',
+      'singlife-Purple-200': '#CFAAD0',
+      'singlife-Orange-800': '#FFA168',
+      'singlife-Orange-200': '#FFD5BE',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
