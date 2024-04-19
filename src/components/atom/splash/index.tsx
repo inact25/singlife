d0da31f555ff877d1ad20a71cf0bd7bd8aefa44c
Angga Pixa <anggas25@live.com>
2024-04-20 00:26:41 +0700

    add splashscreen components

--- src/components/atom/splash/index.tsx
+++ src/components/atom/splash/index.tsx
@@ -2,8 +2,8 @@ import logo from '@assets/svgs/logo.svg'
 
 const Index = () => {
   return (
-    <div className='flex min-h-screen justify-start text-re items-center bg-singlife-red-800'>
-      <img src={logo} alt='splash' />
+    <div className='min-w-[100vw] flex min-h-screen justify-start text-re items-center bg-singlife-red-800'>
+      <img className='m-auto' src={logo} alt='splash' />
     </div>
   )
 }
