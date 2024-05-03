b0a86dd91f5f75b5ced69d4b276cb656a896687a
Angga Pixa <anggas25@live.com>
2024-05-03 21:59:29 +0700

    add popup

--- src/components/atom/button/index.tsx
+++ src/components/atom/button/index.tsx
@@ -12,9 +12,18 @@ const Index = ({ title, type = 'primary', onClick }: Props) => {
     default: 'bg-singlife-purple-800 text-white font-bold',
     warning: 'bg-singlife-orange-800',
   }
+
+  const styleMap = {
+    primary: '0 4px 16px 0 rgba(255, 0, 0, 0.5)',
+    secondary: '',
+    info: 'rgb(25 211 197 / 50%) 0px 4px 16px 0px',
+    default: 'rgb(136 62 137 / 50%) 0px 4px 16px 0px',
+    warning: 'rgb(255 161 104 / 50%) 0px 4px 16px 0px',
+  }
   return (
     <button
       onClick={onClick}
+      style={{ boxShadow: styleMap[type] }}
       className={`w-full px-5 py-3  text-[16px] font-semibold  rounded-full ${btnMap[type]}`}
     >
       {title}
