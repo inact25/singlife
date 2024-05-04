f04096a0ea2896cf589265d42a66664f84e75972
indra <indra.gunanda@gmail.com>
2024-05-05 02:50:35 +0700

    fix(AR) : 360 degree files

--- src/components/WebAR/ArRender.tsx
+++ src/components/WebAR/ArRender.tsx
@@ -5,51 +5,53 @@ import AFrameScene from '@components/libs/AFrameScene'
 
 const ArRender = () => {
   const aframe = useAframe()
-  useEffect(() => {
-    aframe.register('colorize', {
-      schema: {
-        color: { default: 'red' },
-      },
-      init: function () {
-        console.log('init a')
-        const data = this.data
-        const el = this.el // <a-box>
-        el.addEventListener('click', function () {
-          el.setAttribute('color', data.color)
-        })
-      },
-    })
-    aframe.register('colorize-v2', {
-      schema: {
-        color: { default: 'red' },
-      },
-      init: function () {
-        console.log('init v2')
-        const data = this.data
-        const el = this.el // <a-box>
-        el.addEventListener('click', function () {
-          el.setAttribute('color', data.color)
-        })
-      },
-    })
-  }, [])
+  useEffect(() => {}, [])
   return (
     <AFrameScene>
-      <a-box position='-1 0.5 -3' rotation='0 45 0' color='#4CC3D9'></a-box>
-      <a-sphere position='0 1.25 -5' radius='1.25' color='#EF2D5E'></a-sphere>
-      <a-cylinder
-        position='1 0.75 -3'
-        height='1.5'
-        color='#FFC65D'
-      ></a-cylinder>
-      <a-plane
-        position='0 0 -4'
-        rotation='-90 0 0'
-        width='4'
-        height='4'
-        color='#7BC8A4'
-      ></a-plane>
-      <a-sky color='#ECECEC'></a-sky>
+      <a-assets>
+        <audio
+          id='click-sound'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg'
+        ></audio>
+
+        <img
+          id='city'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg'
+        />
+        <img
+          id='city-thumb'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg'
+        />
+        <img
+          id='cubes'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg'
+        />
+        <img
+          id='cubes-thumb'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg'
+        />
+        <img
+          id='sechelt'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg'
+        />
+        <img
+          id='sechelt-thumb'
+          src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg'
+        />
+      </a-assets>
+
+      <a-sky id='image-360' radius='10' src='#city'></a-sky>
+
+      <a-entity className='link'></a-entity>
+
+      <a-camera>
+        <a-cursor
+          id='cursor'
+          animation__click='property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click'
+          animation__clickreset='property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click'
+          animation__fusing='property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing'
+        ></a-cursor>
+      </a-camera>
     </AFrameScene>
   )
 }
