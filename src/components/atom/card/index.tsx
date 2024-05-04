b381c7e43cc61a1d3d92fbd7cca3577bc93214af
Angga Pixa <anggas25@live.com>
2024-05-04 08:49:20 +0700

    add popup

--- src/components/atom/card/index.tsx
+++ src/components/atom/card/index.tsx
@@ -1,11 +1,15 @@
+import CountUp from 'react-countup'
+
 type Props = {
-  count: string
+  count: number
   title: string
 }
 const Index = ({ count, title }: Props) => {
   return (
     <div className='rounded-xl bg-white p-3'>
-      <div className='font-bold text-[40px] text-black'>{count}</div>
+      <div className='font-bold text-[40px] text-black'>
+        <CountUp end={count} duration={5} suffix='+' />
+      </div>
       <div className='font-medium text-[14px] text-black'>{title}</div>
     </div>
   )
