1aeaa326089d86bc0b5307612765c581659de232
indra <indra.gunanda@gmail.com>
2024-05-05 11:19:26 +0700

    fix(AR) : sample files

--- src/components/WebAR/ArRender.tsx
+++ src/components/WebAR/ArRender.tsx
@@ -1,10 +1,40 @@
 // @ts-nocheck
 import useAframe from '@hooks/useAframe.ts'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import AFrameScene from '@components/libs/AFrameScene'
 
-const ArRender = () => {
+type Props = {
+  params: any
+}
+const ArRender: React.FC<Props> = ({ params }) => {
   const aframe = useAframe()
+  const [text, setText] = React.useState('')
+  useEffect(() => {
+    console.log('_params', params?.param)
+    let counter = 0
+    const interval = setInterval(() => {
+      setText(`Counter: ${counter++}`)
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [params])
+  useEffect(() => {
+    aframe.register('linknew', {
+      schema: {
+        on: { type: 'string', default: 'click' },
+        href: { type: 'string' },
+      },
+      init: function () {
+        console.log('loaded link')
+        const data = this.data
+        const el = this.el
+        el.addEventListener(data.on, function () {
+          if (data.href) {
+            window.location.href = data.href
+          }
+        })
+      },
+    })
+  }, [])
   useEffect(() => {}, [])
   return (
     <AFrameScene>
@@ -39,8 +69,8 @@ const ArRender = () => {
           src='https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg'
         />
       </a-assets>
-
-      <a-sky id='image-360' radius='10' src='#city'></a-sky>
+      <a-text value={text}></a-text>
+      <a-sky id='image-360' radius='10' src='#city' linknew></a-sky>
 
       <a-entity className='link'></a-entity>
 
--- src/pages/ar/index.tsx
+++ src/pages/ar/index.tsx
@@ -1,7 +1,14 @@
 import ArRender from '@components/WebAR/ArRender.tsx'
 
 const Ar = () => {
-  return <ArRender />
+  const getAllQS = () => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const allQS = Object.fromEntries(urlParams.entries())
+    return allQS
+  }
+
+  const qs = getAllQS()
+  return <ArRender params={qs} />
 }
 
 export default Ar
