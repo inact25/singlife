1bbea254d934b21afdaa5cd2c84ac461ba690486
indra <indra.gunanda@gmail.com>
2024-05-12 04:26:38 +0700

    fix(ar) : replicate method

--- src/hooks/useAframe.ts
+++ src/hooks/useAframe.ts
@@ -1,15 +1,17 @@
 /* eslint-disable prettier/prettier */
-import { useState } from 'react'
 
+let globalComponent = new Set()
 const useAframe = () => {
-  const [registeredComponents, setRegisteredComponents] = useState(new Set())
   // @ts-ignore
   const AFRAME = window?.AFRAME || null
   const register = (name: string, component: any) => {
-    if (registeredComponents.has(name)) return
     if (AFRAME) {
+      if (globalComponent.has(name)) {
+        console.warn('Component already registered')
+        return
+      }
       AFRAME.registerComponent(name, component)
-      setRegisteredComponents(new Set(registeredComponents.add(name)))
+      globalComponent.add(name)
     }
   }
   return {
