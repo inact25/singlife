fd42f176bf3912e4ed3974f7451dcce3b48b4b98
anggaPixa <anggas25@live.com>
2024-05-17 21:09:45 +0700

    fix swal stuck on open

--- src/utils/bottomPopup/bottomPopup.ts
+++ src/utils/bottomPopup/bottomPopup.ts
@@ -5,8 +5,8 @@ const bottomPopup = () => {
   return Swal.fire({
     customClass: {
       container: 'p-0',
-      popup: 'rounded-b-none rounded-t-[32px]',
-      title: 'body-1 font-medium text-black pt-10',
+      popup: 'rounded-b-none rounded-t-[32px] bg-black/50',
+      title: 'body-1 font-medium text-white pt-10',
       actions: 'w-full px-5',
       confirmButton: 'w-full rounded-full bg-singlife-red-800',
     },
@@ -27,7 +27,7 @@ const bottomPopup = () => {
     position: 'bottom',
     title: 'Your work has been saved',
     showConfirmButton: true,
-    confirmButtonText: 'CREATE YOUR OWN DREAM',
+    confirmButtonText: 'Create your own dream',
     allowOutsideClick: false,
   })
 }
