ee95a3140125c5acab72699337f70299790cc189
unknown <rambudiarto@gmail.com>
2024-05-18 19:43:22 +0700

    add 8thwall

--- src/components/WebAR/ArRender.tsx
+++ src/components/WebAR/ArRender.tsx
@@ -21,9 +21,207 @@ const ArRender: React.FC<Props> = ({ params }) => {
     aframe.register('reality-ready', {
       init: function () {
         const el = this.el
-        el.addEventListener('loaded', function () {
+        el.addEventListener('realityready', function () {
           console.log('loaded')
         })
+      },
+    })
+    aframe.register('portal-camera', {
+      schema: {
+        width: {default: 10},
+        height: {default: 10},
+      },
+      init() {
+        this.camera = this.el
+        this.contents = document.getElementById('portal-contents')
+        this.walls = document.getElementById('hider-walls')
+        this.portalWall = document.getElementById('portal-wall')
+        this.portalVideo = document.getElementById('portalVideo')
+        this.isInPortalSpace = false
+        this.wasOutside = true
+      },
+    
+      tick() {
+        const {position} = this.camera.object3D
+        const isOutside = position.z > 0
+        const withinPortalBounds =
+          position.y < this.data.height && Math.abs(position.x) < this.data.width / 2
+        if (this.wasOutside !== isOutside && withinPortalBounds) {
+          this.isInPortalSpace = !isOutside
+        }
+        this.contents.object3D.visible = this.isInPortalSpace || isOutside
+        this.walls.object3D.visible = !this.isInPortalSpace && isOutside
+        this.portalWall.object3D.visible = this.isInPortalSpace && !isOutside
+        this.portalVideo.object3D.visible = isOutside
+        this.wasOutside = isOutside
+      },
+    })
+    aframe.register('spin', {
+      schema: {
+        speed: {default: 2000},
+        direction: {default: 'normal'},
+        axis: {default: 'y'},
+      },
+      init() {
+        const {el} = this
+        el.setAttribute('animation__spin', {
+          property: `object3D.rotation.${this.data.axis}`,
+          from: 0,
+          to: 360,
+          dur: this.data.speed,
+          dir: this.data.direction,
+          loop: true,
+          easing: 'linear',
+        })
+      },
+    })
+    aframe.register('prompt-flow', {
+      init() {
+        this.prompt = document.getElementById('promptText')
+        this.overlay = document.getElementById('overlay')
+    
+        this.el.sceneEl.addEventListener('realityready', () => {
+          this.overlay.style.display = 'block'
+          this.prompt.innerHTML = 'Tap to Place<br>Moon Portal'
+          this.prompt.classList.add('fly-in')
+        })
+    
+        this.el.addEventListener('dismissPrompt', () => {
+          this.prompt.classList.remove('fly-in')
+          this.prompt.classList.add('fly-out')
+        })
+      },
+    })
+    aframe.register('tap-to-place-portal', {
+      init() {
+        const {sceneEl} = this.el
+        const recenterBtn = document.getElementById('recenterButton')
+    
+        this.camera = document.getElementById('camera')
+        this.contents = document.getElementById('portal-contents')
+        this.walls = document.getElementById('hider-walls')
+        this.portalWall = document.getElementById('portal-wall')
+        this.isInPortalSpace = false
+        this.wasOutside = true
+    
+        const portalHiderRing = this.el.sceneEl.querySelector('#portalHiderRing')
+        const portalRim = this.el.sceneEl.querySelector('#portalRim')
+        const portalVideo = this.el.sceneEl.querySelector('#portalVideo')
+        const portalShadow = this.el.sceneEl.querySelector('#portalShadow')
+    
+        const handleClickEvent = (e) => {
+          if (!e.touches || e.touches.length < 2) {
+            recenterBtn.classList.add('pulse-once')
+            sceneEl.emit('recenter')
+            setTimeout(() => {
+              recenterBtn.classList.remove('pulse-once')
+            }, 200)
+          }
+        }
+    
+        const firstPlaceEvent = (e) => {
+          sceneEl.emit('recenter')
+          sceneEl.emit('dismissPrompt')
+    
+          portalHiderRing.setAttribute('animation__1', {
+            property: 'radius-inner',
+            dur: 1500,
+            from: '0.001',
+            to: '3.5',
+            easing: 'easeOutElastic',
+          })
+    
+          portalRim.setAttribute('animation__2', {
+            property: 'scale',
+            dur: 1500,
+            from: '0.001 0.001 0.001',
+            to: '4.3 4.3 4.3',
+            easing: 'easeOutElastic',
+          })
+    
+          portalVideo.setAttribute('animation__3', {
+            property: 'scale',
+            dur: 1500,
+            from: '0.001 0.001 0.001',
+            to: '7 7 1',
+            easing: 'easeOutElastic',
+          })
+    
+          portalShadow.setAttribute('animation__4', {
+            property: 'scale',
+            dur: 1500,
+            from: '0.001 0.001 0.001',
+            to: '15.5 2 11',
+            easing: 'easeOutElastic',
+          })
+          sceneEl.removeEventListener('click', firstPlaceEvent)
+          recenterBtn.addEventListener('click', handleClickEvent, true)
+        }
+    
+        sceneEl.addEventListener('click', firstPlaceEvent)
+      },
+    })
+    aframe.register('responsive-immersive', {
+      init() {
+        const onAttach = ({sessionAttributes}) => {
+          const hiderWalls = document.getElementById('hider-walls')
+          const scene = this.el
+          const s = sessionAttributes
+          if (
+            !s.cameraLinkedToViewer &&
+            !s.controlsCamera &&
+            !s.fillsCameraTexture &&
+            !s.supportsHtmlEmbedded &&
+            s.supportsHtmlOverlay &&
+            !s.usesMediaDevices &&
+            !s.usesWebXr
+          ) {  // Desktop-specific behavior goes here
+          
+          } else if (
+            s.cameraLinkedToViewer &&
+            s.controlsCamera &&
+            !s.fillsCameraTexture &&
+            s.supportsHtmlEmbedded &&
+            !s.supportsHtmlOverlay &&
+            !s.usesMediaDevices &&
+            s.usesWebXr
+          ) {  // HMD-specific behavior goes here
+            if (this.el.sceneEl.xrSession.environmentBlendMode === 'opaque') {
+              // VR HMD (i.e. Oculus Quest) behavior goes here
+            } else if (this.el.sceneEl.xrSession.environmentBlendMode === 'additive' || 'alpha-blend') {
+              // AR HMD (i.e. Quest 3, Hololens) behavior goes here
+              scene.setAttribute('tap-to-place-portal', '')
+              scene.setAttribute('prompt-flow', '')
+              scene.sceneEl.camera.el.setAttribute('portal-camera', '')
+            }
+          } else if (
+            !s.cameraLinkedToViewer &&
+            !s.controlsCamera &&
+            s.fillsCameraTexture &&
+            !s.supportsHtmlEmbedded &&
+            s.supportsHtmlOverlay &&
+            s.usesMediaDevices &&
+            !s.usesWebXr
+          ) {  // Mobile-specific behavior goes here
+            scene.setAttribute('tap-to-place-portal', '')
+            scene.setAttribute('prompt-flow', '')
+            scene.sceneEl.camera.el.setAttribute('portal-camera', '')
+          }
+        }
+    
+        const onxrloaded = () => {
+          XR8.addCameraPipelineModules([{'name': 'responsiveImmersive', onAttach}])
+        }
+        window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
+      },
+    })
+    aframe.register('auto-play-video', {
+      schema: {
+        video: {type: 'string'},
+      },
+      init() {
+        const v = document.querySelector(this.data.video)
+        v.play()
       },
     })
   }, [])
@@ -32,28 +230,92 @@ const ArRender: React.FC<Props> = ({ params }) => {
   }
   return (
     <>
-      <AFrameScene reality-ready>
+    <div id="overlay" style="display: none" className="absolute-fill">
+      <img id="recenterButton" src="https://static.8thwall.app/assets/recenter-m71fa5ubcu.png" /> 
+      <span id="promptText"></span>
+    </div>
+      <AFrameScene 
+      xrextras-loading
+      xrextras-runtime-error
+      renderer="colorManagement: true;"
+      xrweb="allowedDevices: any; disableDefaultEnvironment: true;"
+       reality-ready>
         <a-assets>
-          <img crossOrigin='anonymous' id='city' src={url} />
+          <img crossOrigin='anonymous' id='skybox-img' src={url} />
+
+          {/* portal assets */}
+          <a-asset-item id="portal-rim-model" src="https://static.8thwall.app/assets/portal%20(1)-8fj5kci5t4.glb"></a-asset-item>
+          <a-asset-item id="moon-model" src="https://static.8thwall.app/assets/moon-ground-5dc3hau3o2.glb"></a-asset-item>
+          <a-asset-item id="platform-model" src="https://static.8thwall.app/assets/platform-cvp8cpu7n7.glb"></a-asset-item>
+          <a-asset-item id="flag-model" src="https://static.8thwall.app/assets/flag%20(1)-1gepdc4al8.glb"></a-asset-item>
+          <a-asset-item id="rocks-model" src="https://static.8thwall.app/assets/rocks-1phpku57lc.glb"></a-asset-item>
+          <img id="blob-shadow-img" src="https://static.8thwall.app/assets/blob-shadow-5ez3hboap2.png" />
+          <img id="satellite-img" src="https://static.8thwall.app/assets/satellite-92l691o6f5.png" />
+          <video id="portal-video" muted loop="true" src="https://static.8thwall.app/assets/blue-portal-lrmelmi6qc.mp4"></video>
         </a-assets>
-        <a-box
-          id='box'
-          class='cantap'
-          position='0 2 -5'
-          color='#6173F4'
-          rotation='0 45 45'
-          opacity=' 0.8'
-          depth='1'
-          linknew
-        ></a-box>
-
-        <a-sky id='image-360' radius='10' src='#city'></a-sky>
-
+        
         <a-camera
+          id="camera"
+          position="0 9 11"
           raycaster='objects: .cantap'
-          id='camera'
           cursor='fuse: false; rayOrigin: mouse;'
         ></a-camera>
+
+  <xrextras-opaque-background remove="true">
+    <a-entity id="hider-walls">
+      <a-box scale="100 1 100" position="0 -1 49" xrextras-hider-material></a-box>
+      <a-box scale="100 100 1" position="0 50 75" xrextras-hider-material></a-box>
+      <a-box scale="100 1 100" position="0 100 49" xrextras-hider-material></a-box>
+      <a-box scale="1 100 100" position="-30 50 50" xrextras-hider-material></a-box>
+      <a-box scale="1 100 100" position="30 50 50" xrextras-hider-material></a-box>
+      <a-ring id="portalHiderRing" radius-inner="0.001" radius-outer="100" position="0 7.5 -0.2" xrextras-hider-material></a-ring>
+    </a-entity>
+  
+     <a-entity id="portal-wall">
+      <a-circle radius="5.2" rotation="0 180 0" position="0 7.5 0" scale="0.8 0.8 0" xrextras-hider-material></a-circle>
+      <a-circle radius="5.2" rotation="0 180 0" position="0 7.5 0.25" scale="0.8 0.8 0" xrextras-hider-material></a-circle>
+    </a-entity>
+  </xrextras-opaque-background>
+
+  <a-entity
+    light="
+      type: directional;
+      castShadow: true;
+      shadowMapHeight:2048;
+      shadowMapWidth:2048;
+      shadowCameraTop: 35;
+      shadowCameraBottom: -20;
+      shadowCameraRight: 40;
+      shadowCameraLeft: -10;
+      target: #portalRim"
+    xrextras-attach="target: portalRim; offset: 18 7 14"
+    shadow>
+  </a-entity>
+  
+  <a-light type="ambient" intensity="0.3"></a-light>
+
+  <a-entity id="portal-contents">
+      
+    <a-entity
+      gltf-model="#moon-model"
+      rotation="0 90 0"
+      position="8 -0.5 -5"
+      scale="0.5 0.5 0.5"
+      shadow="cast: false">
+    </a-entity>
+
+    <a-entity
+      gltf-model="#platform-model"
+      rotation="0 -90 0"
+      reflections="type: static"
+      position="-0.4 0 -10"
+      scale="9 9 9"
+      shadow="receive: false">
+    </a-entity>
+    
+     <a-sky id='image-360' radius='10' src='#portal-content'></a-sky>
+    </a-entity>
+        
       </AFrameScene>
     </>
   )
