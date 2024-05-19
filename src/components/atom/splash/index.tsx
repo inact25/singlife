7d414eaffd12ef8fe2726c4214f3f288bfbcad22
Angga Pixa <anggas25@live.com>
2024-05-19 10:49:11 +0700

    add splash screen

--- src/components/atom/splash/index.tsx
+++ src/components/atom/splash/index.tsx
@@ -1,10 +1,19 @@
 import logo from '@assets/svgs/logo.svg'
+import WrapperLayouts from '../../../layouts/wrapper/wrapper.layouts.tsx'
 
 const Index = () => {
   return (
-    <div className='min-w-[100vw] flex min-h-screen justify-start text-re items-center bg-singlife-red-800'>
-      <img className='m-auto' src={logo} alt='splash' />
-    </div>
+    <WrapperLayouts isFull={true}>
+      <div className='min-w-[100vw] flex min-h-screen justify-center text-re items-center bg-singlife-red-800'>
+        <div>
+          <p className='body-1 mb-2 text-white mt-[-2rem]'>Welcome to</p>
+          <h3 className='font-bold mb-10 text-white'>
+            The Singlife Dream Cube
+          </h3>
+          <img className='m-auto' src={logo} alt='splash' />
+        </div>
+      </div>
+    </WrapperLayouts>
   )
 }
 
--- src/pages/introduction/index.tsx
+++ src/pages/introduction/index.tsx
@@ -4,14 +4,16 @@ import Button from '@components/atom/button'
 import Buttonimage from '@components/atom/buttonimage'
 import { useNavigate } from 'react-router-dom'
 import useDream from '@services/api/dream'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useResponsive from '../../hooks/useResponsive.ts'
 import Freedom from '@assets/svgs/Your Financial Freedom Dream.svg'
 import Envision from '@assets/svgs/Envision.svg'
+import Splash from '@components/atom/splash'
 
 const Introduction = () => {
   const responsive = useResponsive()
   const { breakpoint } = responsive
+  const [isSplash, setIsSplash] = useState(true)
   const classList = {
     sm: {
       image: 'w-full absolute',
@@ -44,84 +46,92 @@ const Introduction = () => {
   }
 
   useEffect(() => {
+    setTimeout(() => setIsSplash(false), 3000)
+  }, [])
+
+  useEffect(() => {
     dream_v1.getLatestDreamDo()
   }, [dream_v1.paginate.filter])
   return (
     <div>
-      <WrapperLayouts isFull={true}>
-        <div
-          className='overflow-hidden min-h-screen'
-          style={{
-            background: `url(${background}) top center no-repeat`,
-            backgroundSize: 'cover',
-          }}
-        >
-          <WrapperLayouts>
-            <div className='head w-full max-h-[15vh] pt-5 '>
-              <div className='text-left text-black absolute'>
-                <div className='tagline mt-4'>
-                  <div className='tagline mb-5 relative start-0'>
-                    <img src={Envision} alt={'freedom'} />
+      {isSplash ? (
+        <Splash />
+      ) : (
+        <WrapperLayouts isFull={true}>
+          <div
+            className='overflow-hidden min-h-screen'
+            style={{
+              background: `url(${background}) top center no-repeat`,
+              backgroundSize: 'cover',
+            }}
+          >
+            <WrapperLayouts>
+              <div className='head w-full max-h-[15vh] pt-5 '>
+                <div className='text-left text-black absolute'>
+                  <div className='tagline mt-4'>
+                    <div className='tagline mb-5 relative start-0'>
+                      <img src={Envision} alt={'freedom'} />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </WrapperLayouts>
-          <WrapperLayouts className={classList[breakpoint ?? 'sm'].footer}>
-            <div className='footer'>
-              <div className='tagline mb-5 relative end-0 flex justify-end'>
-                <img src={Freedom} alt={'freedom'} />
-              </div>
-              <div className='action'>
-                <div className='mb-3'>
-                  <Button
-                    onClick={handleGetStarted}
-                    title='Get Started'
-                    type='primary'
-                  />
+            </WrapperLayouts>
+            <WrapperLayouts className={classList[breakpoint ?? 'sm'].footer}>
+              <div className='footer'>
+                <div className='tagline mb-5 relative end-0 flex justify-end'>
+                  <img src={Freedom} alt={'freedom'} />
                 </div>
-                {/*<div className='mb-3'>*/}
-                {/*  <Button*/}
-                {/*    onClick={() => console.log('')}*/}
-                {/*    title={*/}
-                {/*      <span className='flex items-center gap-2 justify-center'>*/}
-                {/*        <span className='text-[18px]'>*/}
-                {/*          <RiChatSmile3Fill />*/}
-                {/*        </span>{' '}*/}
-                {/*        Talk to a Financial Advisor*/}
-                {/*      </span>*/}
-                {/*    }*/}
-                {/*    type='secondary'*/}
-                {/*  />*/}
-                {/*</div>*/}
-                {/*<div className='divider my-2 grid items-center grid-cols-10'>*/}
-                {/*  <span className={'col-span-4'}>*/}
-                {/*    <hr />*/}
-                {/*  </span>*/}
-                {/*  <span*/}
-                {/*    className={*/}
-                {/*      'font-semibold text-black text-[14px] col-span-2'*/}
-                {/*    }*/}
-                {/*  >*/}
-                {/*    Or*/}
-                {/*  </span>*/}
-                {/*  <span className={'col-span-4'}>*/}
-                {/*    <hr />*/}
-                {/*  </span>*/}
-                {/*</div>*/}
-                <div className='mb-3'>
-                  <Buttonimage
-                    title={'Explore Dream Gallery'}
-                    type='secondary'
-                    onClick={handleExploreDreamGallery}
-                    images={dream_v1.latestDream.map((item) => item.image)}
-                  />
+                <div className='action'>
+                  <div className='mb-3'>
+                    <Button
+                      onClick={handleGetStarted}
+                      title='Get Started'
+                      type='primary'
+                    />
+                  </div>
+                  {/*<div className='mb-3'>*/}
+                  {/*  <Button*/}
+                  {/*    onClick={() => console.log('')}*/}
+                  {/*    title={*/}
+                  {/*      <span className='flex items-center gap-2 justify-center'>*/}
+                  {/*        <span className='text-[18px]'>*/}
+                  {/*          <RiChatSmile3Fill />*/}
+                  {/*        </span>{' '}*/}
+                  {/*        Talk to a Financial Advisor*/}
+                  {/*      </span>*/}
+                  {/*    }*/}
+                  {/*    type='secondary'*/}
+                  {/*  />*/}
+                  {/*</div>*/}
+                  {/*<div className='divider my-2 grid items-center grid-cols-10'>*/}
+                  {/*  <span className={'col-span-4'}>*/}
+                  {/*    <hr />*/}
+                  {/*  </span>*/}
+                  {/*  <span*/}
+                  {/*    className={*/}
+                  {/*      'font-semibold text-black text-[14px] col-span-2'*/}
+                  {/*    }*/}
+                  {/*  >*/}
+                  {/*    Or*/}
+                  {/*  </span>*/}
+                  {/*  <span className={'col-span-4'}>*/}
+                  {/*    <hr />*/}
+                  {/*  </span>*/}
+                  {/*</div>*/}
+                  <div className='mb-3'>
+                    <Buttonimage
+                      title={'Explore Dream Gallery'}
+                      type='secondary'
+                      onClick={handleExploreDreamGallery}
+                      images={dream_v1.latestDream.map((item) => item.image)}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          </WrapperLayouts>
-        </div>
-      </WrapperLayouts>
+            </WrapperLayouts>
+          </div>
+        </WrapperLayouts>
+      )}
     </div>
   )
 }
