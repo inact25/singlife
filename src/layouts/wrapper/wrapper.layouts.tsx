6f0b3e9027fc918635b05aad3ddae6a2be685d52
Angga Pixa <anggas25@live.com>
2024-05-19 13:30:34 +0700

    add detector

--- src/layouts/wrapper/wrapper.layouts.tsx
+++ src/layouts/wrapper/wrapper.layouts.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from 'react'
-import useResponsive from '../../hooks/useResponsive.ts'
-import QrCodeCreator from '@components/molecules/QrCodeCreator'
 
 type Props = {
   children: ReactNode
@@ -13,31 +11,29 @@ const WrapperLayouts = ({
   children,
   isFull = false,
   className,
-  allDevice,
+  // allDevice,
 }: Props) => {
-  const host = window.location.origin
-  const responsive = useResponsive()
-  if (
-    responsive.breakpoint === 'xs' ||
-    responsive.breakpoint === 'sm' ||
-    allDevice
-  ) {
-    return (
-      <div className={`w-full  ${!isFull && 'p-5'} ${className}`}>
-        {children}
-      </div>
-    )
-  }
+  // const host = window.location.origin
+  // const responsive = useResponsive()
+  // if (
+  //   responsive.breakpoint === 'xs' ||
+  //   responsive.breakpoint === 'sm' ||
+  //   allDevice
+  // ) {
   return (
-    <div className='text-black rounded-3xl border-2 border-black m-auto max-w-sm mt-16 min-h-[80vh] flex-col flex items-center justify-center gap-2'>
-      <QrCodeCreator value={host} />
-      <div className='w-full'>
-        <h4 className='text-center' style={{ fontSize: '1.5rem' }}>
-          Scan the QR code to view the mobile version
-        </h4>
-      </div>
-    </div>
+    <div className={`w-full  ${!isFull && 'p-5'} ${className}`}>{children}</div>
   )
 }
+//   return (
+//     <div className='text-black rounded-3xl border-2 border-black m-auto max-w-sm mt-16 min-h-[80vh] flex-col flex items-center justify-center gap-2'>
+//       <QrCodeCreator value={host} />
+//       <div className='w-full'>
+//         <h4 className='text-center' style={{ fontSize: '1.5rem' }}>
+//           Scan the QR code to view the mobile version
+//         </h4>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default WrapperLayouts
