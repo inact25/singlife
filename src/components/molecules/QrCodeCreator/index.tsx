d823c1bb2a2d16422f2d69110b4778e11217a767
anggaPixa <anggas25@live.com>
2024-05-25 13:33:38 +0700

    fix border

--- src/components/molecules/QrCodeCreator/index.tsx
+++ src/components/molecules/QrCodeCreator/index.tsx
@@ -7,9 +7,10 @@ type Props = {
 const QrCodeCreator = ({ value }: Props) => {
   return (
     <QRCode
-      className='rounded-md'
-      fgColor='#FF0000'
-      bgColor='#ffe9ec'
+        style={{boxShadow: 'rgba(255, 0, 8, 0.2) 0px 7px 29px 0px'}}
+        className='h-[165px] w-[165px] p-5 bg-white rounded-xl shadow-singlife-red-800'
+        fgColor='black'
+        bgColor='white'
       value={value}
     />
   )
--- src/layouts/wrapper/wrapper.layouts.tsx
+++ src/layouts/wrapper/wrapper.layouts.tsx
@@ -1,4 +1,9 @@
-import { ReactNode } from 'react'
+import {ReactNode} from 'react'
+import QrCodeCreator from "@components/molecules/QrCodeCreator";
+import useResponsive from "@hooks/useResponsive.ts";
+import mainImg from "@assets/svgs/main.svg"
+import Button from "@components/atom/button";
+import {RiChatSmile2Fill} from "react-icons/ri";
 
 type Props = {
   children: ReactNode
@@ -11,29 +16,37 @@ const WrapperLayouts = ({
   children,
   isFull = false,
   className,
-  // allDevice,
+                          allDevice,
 }: Props) => {
-  // const host = window.location.origin
-  // const responsive = useResponsive()
-  // if (
-  //   responsive.breakpoint === 'xs' ||
-  //   responsive.breakpoint === 'sm' ||
-  //   allDevice
-  // ) {
+  const host = window.location.origin
+  const responsive = useResponsive()
+  if (
+      responsive.breakpoint === 'xs' ||
+      responsive.breakpoint === 'sm' ||
+      allDevice
+  ) {
   return (
     <div className={`w-full  ${!isFull && 'p-5'} ${className}`}>{children}</div>
   )
 }
-//   return (
-//     <div className='text-black rounded-3xl border-2 border-black m-auto max-w-sm mt-16 min-h-[80vh] flex-col flex items-center justify-center gap-2'>
-//       <QrCodeCreator value={host} />
-//       <div className='w-full'>
-//         <h4 className='text-center' style={{ fontSize: '1.5rem' }}>
-//           Scan the QR code to view the mobile version
-//         </h4>
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+      <div className='flex min-h-screen justify-center items-center'>
+        <div>
+          <div className='min-h-[680px] min-w-[680px] flex justify-center items-center'
+               style={{background: `url(${mainImg})`}}>
+            <div className='ml-2 mt-[6rem]'>
+              <div className='mx-auto flex justify-center'>
+                <QrCodeCreator value={host}/>
+              </div>
+              <div className='mt-28 ml-16'>
+                <Button onClick={()=>window.open('https://singlife.com/','_blank')} title={<div className='flex justify-center items-center gap-5'><RiChatSmile2Fill/>Talk to a
+                  Financial Freedom</div>}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
 
 export default WrapperLayouts
