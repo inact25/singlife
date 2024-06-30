fcf9270a92c7542cbec251b53687563fa6855fcb
Angga Pixa <anggas25@live.com>
2024-06-30 13:27:21 +0700

    add delay 3 detik

--- src/components/atom/mediapop/index.tsx
+++ src/components/atom/mediapop/index.tsx
@@ -3,83 +3,105 @@ import l3602 from '@assets/svgs/l3602.svg'
 import waiting from '@assets/svgs/waiting.svg'
 import walk from '@assets/svgs/walk.svg'
 import tap from '@assets/svgs/tap.svg'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 type Props = {
-    type?: '360' | 'waiting' | 'tap' | 'move'
-    className?: string
-    show?: boolean,
-    onClick?: () => void
+  type?: '360' | 'waiting' | 'tap' | 'move' | 'loader'
+  className?: string
+  show?: boolean
+  onClick?: () => void
 }
-const MediaPopup = ({type = '360', className, show = true, onClick}: Props) => {
+
+const getMessageByType = (type: string = ''): string => {
+  const messages: { [key: string]: string } = {
+    '360': 'Look around to explore',
+    waiting: 'Loading your dream world',
+    move: 'Look around to explore',
+    loader:
+      'For the best experience, be in a well-lit and spacious environment. If loading is slow, please refresh your browser.',
+  }
+
+  return messages[type] || 'Tap the floor to start'
+}
+
+const MediaPopup = ({
+  type = '360',
+  className,
+  show = true,
+  onClick,
+}: Props) => {
+  const message = getMessageByType(type)
   return (
-      <motion.div
-          onClick={onClick}
-          style={{display: show ? 'block' : 'none'}}
-          initial={{scale: .75, opacity: 0}}
-          animate={{scale: 1, opacity: 1}}
-          transition={{
-            type: "spring",
-            stiffness: 260,
-            damping: 20
-          }}
-          className={`rounded-2xl flex flex-wrap justify-center items-center px-5 py-10 bg-white/40 backdrop-blur-md ${className}`}
+    <motion.div
+      onClick={onClick}
+      style={{ display: show ? 'block' : 'none' }}
+      initial={{ scale: 0.75, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+      }}
+      className={`rounded-2xl flex flex-wrap justify-center items-center px-5 py-10 bg-white/40 backdrop-blur-md ${className}`}
     >
-          {type === 'move' && (
-              <div className='w-full mb-6'>
-                  <div className='mb-5'>
-                      <motion.img
-                          animate={{
-                              scale: [.95, 1, .95]
-                          }}
-                          transition={{
-                              repeat: Infinity,
-                              duration: 2,
-                          }}
-                          className='m-auto'
-                          width={88}
-                          height={88}
-                          src={walk}
-                          alt='l360'
-                      />
-                  </div>
-                  <div>
-                      <p className='note text-black'>
-                          Move forward <br/> to enter the portal
-                      </p>
-                  </div>
-              </div>
-
-          )}
-
-      <div className='w-full'>
-        <div className='mb-5'>
-          <motion.img
+      {type === 'move' && (
+        <div className='w-full mb-6'>
+          <div className='mb-5'>
+            <motion.img
               animate={{
-                scale: [.95, 1, .95]
+                scale: [0.95, 1, 0.95],
               }}
               transition={{
                 repeat: Infinity,
                 duration: 2,
               }}
+              className='m-auto'
+              width={88}
+              height={88}
+              src={walk}
+              alt='l360'
+            />
+          </div>
+          <div>
+            <p className='note text-black'>
+              Move forward <br /> to enter the portal
+            </p>
+          </div>
+        </div>
+      )}
+
+      <div className='w-full'>
+        <div className='mb-5'>
+          <motion.img
+            animate={{
+              scale: [0.95, 1, 0.95],
+            }}
+            transition={{
+              repeat: Infinity,
+              duration: 2,
+            }}
             className='m-auto'
             width={88}
             height={88}
-              src={type === '360' ? l360 : type === 'waiting' ? waiting : type === 'move' ? l3602 : tap}
+            src={
+              type === '360'
+                ? l360
+                : type === 'waiting'
+                  ? waiting
+                  : type === 'move'
+                    ? l3602
+                    : type === 'loader'
+                      ? waiting
+                      : tap
+            }
             alt='l360'
           />
         </div>
         <div>
-          <p className='note text-black'>
-            {type === '360'
-              ? 'Look around to explore'
-              : type === 'waiting'
-                ? 'Loading your dream world'
-                    : type === "move" ? 'Look around to explore' : 'Tap the floor to start'}
-          </p>
+          <p className='note text-black'>{message}</p>
         </div>
       </div>
-      </motion.div>
+    </motion.div>
   )
 }
 
--- src/pages/question/RenderQuestion.tsx
+++ src/pages/question/RenderQuestion.tsx
@@ -11,14 +11,13 @@ import WrapperLayouts from '../../layouts/wrapper/wrapper.layouts.tsx'
 import Slidedot from '@components/atom/slidedot'
 import Radio from '@components/atom/radio'
 import Button from '@components/atom/button'
-import React, {useEffect, useState} from 'react'
-import {ListQuiz, QuizSubmitItem} from '@services/api/quiz/type'
+import React, { useEffect, useState } from 'react'
+import { ListQuiz, QuizSubmitItem } from '@services/api/quiz/type'
 import htmlParser from 'html-react-parser'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import useQuiz from '@services/api/quiz'
 import womenLeader from '@assets/lottie/Woman on ladder.json'
-import loading from '@assets/lottie/loading.json'
-import {Player} from '@lottiefiles/react-lottie-player'
+import { Player } from '@lottiefiles/react-lottie-player'
 
 type Props = {
   selected: number
@@ -93,7 +92,6 @@ const RenderQuestion: React.FC<Props> = ({
   const quiz_service = useQuiz()
   const [record, setRecord] = useState<ListQuiz | null>(null)
   const [isActive, setIsActive] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
 
   const isChecked = (id: number) => {
     return value?.answer_id === id
@@ -113,14 +111,6 @@ const RenderQuestion: React.FC<Props> = ({
   useEffect(() => {
     if (selected) {
       quiz_service.getQuizRPCDo(selected, lastChoice ?? 0)
-
-      const timeOut = setTimeout(() => {
-        setIsLoading(false);
-      }, 2500);
-
-      return () => {
-        clearTimeout(timeOut);
-      };
     }
   }, [selected])
 
@@ -135,161 +125,154 @@ const RenderQuestion: React.FC<Props> = ({
   }
 
   return (
-      <>
-        {isLoading ?
-            <div className='flex h-[100dvh] justify-center items-center'>
-                <Player
+    <>
+      <div className=''>
+        <div className='absolute top-5 left-5 text-left z-10'>
+          <Buttonicon
+            icon={back}
+            onClick={() => handleBack && handleBack(record.question_id - 1)}
+          />
+        </div>
+        <div
+          style={{
+            backgroundSize: 'cover',
+            background: `url(${switchImage(index)}) center center no-repeat`,
+          }}
+          className='image-screen w-screen min-h-[100dvh] flex items-center'
+        >
+          <div className='w-full'>
+            <div
+              className={` ${index === 0 ? 'mt-[-15rem]' : index === 1 ? 'absolute -top-10 right-0' : 'absolute women-ladder right-0'}`}
+            >
+              {index === 2 && (
+                <div>
+                  <Player
                     autoplay={true}
                     loop={true}
                     controls={false}
-                    src={loading}
-                    style={{height: 256, width: 256}}
-                />
-            </div> :
-            <div className=''>
-      <div className='absolute top-5 left-5 text-left z-10'>
-        <Buttonicon
-          icon={back}
-          onClick={() => handleBack && handleBack(record.question_id - 1)}
-        />
-      </div>
-      <div
-        style={{
-          backgroundSize: 'cover',
-          background: `url(${switchImage(index)}) center center no-repeat`,
-        }}
-        className='image-screen w-screen min-h-[100dvh] flex items-center'
-      >
-        <div className='w-full'>
-          <div
-            className={` ${index === 0 ? 'mt-[-15rem]' : index === 1 ? 'absolute -top-10 right-0' : 'absolute women-ladder right-0'}`}
-          >
-            {index === 2 && (
-              <div>
-                <Player
-                  autoplay={true}
-                  loop={true}
-                  controls={false}
-                  src={womenLeader}
-                  style={{ height: '100%', width: '100vw', marginLeft: 'auto' }}
-                />
-              </div>
-            )}
-            {index === 1 && (
+                    src={womenLeader}
+                    style={{
+                      height: '100%',
+                      width: '100vw',
+                      marginLeft: 'auto',
+                    }}
+                  />
+                </div>
+              )}
+              {index === 1 && (
                 <div className={'flex justify-between w-[100dvw]  gap-10'}>
-                  <div style={{marginTop:'10rem'}}>
+                  <div style={{ marginTop: '10rem' }}>
                     <motion.img
-                        transition={{
-                          y: motionImageConfig.transition.y,
-                        }}
-                        animate={{
-                          y: motionImageConfig.animate.y,
-                        }}
-                        src={object2}
-                        alt={''}
-                        style={{maxWidth: '50vw'}}
-                    />
-                  </div>
-                  <motion.img
                       transition={{
                         y: motionImageConfig.transition.y,
                       }}
                       animate={{
                         y: motionImageConfig.animate.y,
                       }}
-                      src={object3}
+                      src={object2}
                       alt={''}
-                      style={{maxWidth: '50vw'}}
+                      style={{ maxWidth: '50vw' }}
+                    />
+                  </div>
+                  <motion.img
+                    transition={{
+                      y: motionImageConfig.transition.y,
+                    }}
+                    animate={{
+                      y: motionImageConfig.animate.y,
+                    }}
+                    src={object3}
+                    alt={''}
+                    style={{ maxWidth: '50vw' }}
                   />
                 </div>
-            )}
-            {index !== 2 && index !== 1 && (
-              <motion.img
-                transition={{
-                  y: motionImageConfig.transition.y,
-                }}
-                animate={{
-                  y: motionImageConfig.animate.y,
-                }}
-                src={
-                  index === 0
-                    ? object1
-                    : index === 1
-                      ? object3
-                      : object4
-                }
-                alt={''}
-                style={{ maxWidth: '100vw' }}
-              />
-            )}
-          </div>
-          <div
-            className={`content absolute bottom-0 text-start bg-white rounded-t-[32px] w-full`}
-          >
-            <WrapperLayouts>
-              <h2
-                className={`title text-black w-[300px] ${isActive ? 'custom-popup' : 'pb-0'}`}
-              >
-                {htmlParser(record.question)}
-                {!isActive && (
-                  <div className='w-full custom-padding-quiz overflow-hidden'>
-                    <Slidedot
-                      position={'center'}
-                      color={colorPicker(index)}
-                      indexActive={index}
-                    />
-                  </div>
-                )}
-              </h2>
-              <motion.div
-                className='content'
-                initial={'closed'}
-                animate={isActive ? 'open' : 'closed'}
-                variants={motionConfig}
-              >
-                {record.choices.map((item) => (
-                  <div className='flex gap-3 items-center mb-3' key={item.c_id}>
-                    <div>
-                      <Radio
+              )}
+              {index !== 2 && index !== 1 && (
+                <motion.img
+                  transition={{
+                    y: motionImageConfig.transition.y,
+                  }}
+                  animate={{
+                    y: motionImageConfig.animate.y,
+                  }}
+                  src={index === 0 ? object1 : index === 1 ? object3 : object4}
+                  alt={''}
+                  style={{ maxWidth: '100vw' }}
+                />
+              )}
+            </div>
+            <div
+              className={`content absolute bottom-0 text-start bg-white rounded-t-[32px] w-full`}
+            >
+              <WrapperLayouts>
+                <h2
+                  className={`title text-black w-[300px] ${isActive ? 'custom-popup' : 'pb-0'}`}
+                >
+                  {htmlParser(record.question)}
+                  {!isActive && (
+                    <div className='w-full custom-padding-quiz overflow-hidden'>
+                      <Slidedot
+                        position={'center'}
                         color={colorPicker(index)}
-                        type='round'
-                        checked={isChecked(item.c_id)}
-                        onChange={() => {
-                          onChange &&
-                            onChange({
-                              quiz_id: record?.question_id,
-                              answer_id: item.c_id,
-                            })
-                        }}
+                        indexActive={index}
                       />
                     </div>
-                    <div>
-                      <p className='body-1 text-black'>{item.c_text}</p>
+                  )}
+                </h2>
+                <motion.div
+                  className='content'
+                  initial={'closed'}
+                  animate={isActive ? 'open' : 'closed'}
+                  variants={motionConfig}
+                >
+                  {record.choices.map((item) => (
+                    <div
+                      className='flex gap-3 items-center mb-3'
+                      key={item.c_id}
+                    >
+                      <div>
+                        <Radio
+                          color={colorPicker(index)}
+                          type='round'
+                          checked={isChecked(item.c_id)}
+                          onChange={() => {
+                            onChange &&
+                              onChange({
+                                quiz_id: record?.question_id,
+                                answer_id: item.c_id,
+                              })
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <p className='body-1 text-black'>{item.c_text}</p>
+                      </div>
+                    </div>
+                  ))}
+                  <div className='grid grid-cols-12 gap-3 items-center pt-[24px]'>
+                    <div className='col-span-4'>
+                      <Slidedot
+                        indexActive={index}
+                        color={colorPicker(index)}
+                      />
+                    </div>
+                    <div className='col-span-8'>
+                      <Button
+                        disabled={value?.answer_id === 0}
+                        type='default'
+                        title={isLast ? 'Submit' : 'Next'}
+                        onClick={isLast ? handleSubmit : handleNext}
+                        color={colorPicker(index)}
+                      />
                     </div>
                   </div>
-                ))}
-                <div className='grid grid-cols-12 gap-3 items-center pt-[24px]'>
-                  <div className='col-span-4'>
-                    <Slidedot indexActive={index} color={colorPicker(index)} />
-                  </div>
-                  <div className='col-span-8'>
-                    <Button
-                      disabled={value?.answer_id === 0}
-                      type='default'
-                      title={isLast ? 'Submit' : 'Next'}
-                      onClick={isLast ? handleSubmit : handleNext}
-                      color={colorPicker(index)}
-                    />
-                  </div>
-                </div>
-              </motion.div>
-            </WrapperLayouts>
+                </motion.div>
+              </WrapperLayouts>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-        }
-      </>
+    </>
   )
 }
 
